06895c10dd61e32934157d54f45fc62484c086bc
Looney .exe <dmytro1601@gmail.com>
2026-05-07 01:10:25 +0200

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,31 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    assert sum(split_integer(6, 3)) == 6
+    assert sum(split_integer(8, 4)) == 8
+    assert sum(split_integer(6, 2)) == 6
+    assert sum(split_integer(8, 1)) == 8
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    assert split_integer(6, 3) == [2, 2, 2]
+    assert split_integer(8, 4) == [2, 2, 2, 2]
+    assert split_integer(9, 3) == [3, 3, 3]
+    assert split_integer(16, 1) == [16]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    assert split_integer(6, 1) == [6]
+    assert split_integer(10, 1) == [10]
+    assert split_integer(50, 1) == [50]
+    assert split_integer(100, 1) == [100]
+
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    result = split_integer(17, 4)
+    assert result == sorted(result)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    assert split_integer(1, 3) == [0, 0, 1]
