b63ac185ab6fa40159748eae649a3a3c323e846a
Looney .exe <dmytro1601@gmail.com>
2026-05-07 01:26:28 +0200

    Solution3

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -9,16 +9,17 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    result = split_integer(6, 3)
-    assert sum(result) == 6
-    assert len(result) == 3
-    assert result == sorted(result)
-    assert max(result) - min(result) <= 1
+    assert split_integer(6, 3) == [2, 2, 2]
+    assert split_integer(8, 4) == [2, 2, 2, 2]
+    assert split_integer(9, 3) == [3, 3, 3]
+    assert split_integer(16, 1) == [16]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert split_integer(6, 1) == [6]
     assert split_integer(10, 1) == [10]
+    assert split_integer(50, 1) == [50]
+    assert split_integer(100, 1) == [100]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
@@ -27,8 +28,4 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    result = split_integer(1, 3)
-    assert len(result) == 3
-    assert sum(result) == 1
-    assert result == sorted(result)
-    assert max(result) - min(result) <= 1
+    assert split_integer(1, 3) == [0, 0, 1]
